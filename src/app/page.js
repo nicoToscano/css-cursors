b4e490eed024863b4bf82c87371812d615a07a9c
Nicolás Toscano <nicolastoscano1@hotmail.com>
2025-05-03 00:43:03 -0500

    Refactor Home component to display CSS cursor properties with updated layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,38 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <h1 className="title">CSS Cursor Property</h1>
+      <div className="container">
+        <div className="card card1">crosshair</div>
+        <div className="card card2">help</div>
+        <div className="card card3">move</div>
+        <div className="card card4">pointer</div>
+        <div className="card card5">progress</div>
+        <div className="card card6">text</div>
+        <div className="card card7">wait</div>
+        <div className="card card8">vertical-text</div>
+        <div className="card card9">alias</div>
+        <div className="card card10">no-drop</div>
+        <div className="card card11">grab</div>
+        <div className="card card12">grabbing</div>
+        <div className="card card13">zoom-in</div>
+        <div className="card card14">zoom-out</div>
+        <div className="card card15">not-allowed</div>
+        <div className="card card16">cell</div>
+        <div className="card card17">copy</div>
+        <div className="card card18">ew-resize</div>
+        <div className="card card19">ns-resize</div>
+        <div className="card card20">nwse-resize</div>
+        <div className="card card21">nesw-resize</div>
+        <div className="card card22">col-resize</div>
+        <div className="card card23">row-resize</div>
+        <div className="card card24">all-scroll</div>
+        <div className="card card25">context-menu</div>
+        <div className="card card26">none</div>
+      </div>
+    </>
   );
 }
